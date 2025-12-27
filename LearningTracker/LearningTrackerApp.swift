967275f27c44b1be61f31b9500625a02dba7aad3
wannabemountainking <kdy0096@gmail.com>
2025-12-27 14:47:10 +0900

    Initial Commit

---
+++ LearningTracker/LearningTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningTrackerApp.swift
+//  LearningTracker
+//
+//  Created by YoonieMac on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
